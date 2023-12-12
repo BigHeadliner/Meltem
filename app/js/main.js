c2699e948a50a5485ae06c3732ce259eeb4c889c
BigHeadliner <bigheadliner@gmail.com>
2023-12-12 21:11:58 +0200

    main section ready

--- app/js/main.js
+++ app/js/main.js
@@ -2,6 +2,38 @@ $(function () {
      
     
  
+    function setProgress(index) {
+        const calc = ((index + 1) / ($slider.slick('getSlick').slideCount)) * 100;
+      
+        $progressBar
+          .css('background-size', `${calc}% 100%`)
+          .attr('aria-valuenow', calc);
+      
+        $progressBarLabel.text(`${calc.toFixed(2)}% completed`);
+      }
+      
+      const $slider = $('.main__slider');
+      const $progressBar = $('.progress');
+      const $progressBarLabel = $('.slider__label');
+      
+      $slider.on('beforeChange', function(event, slick, currentSlide, nextSlide) {   
+        setProgress(nextSlide);
+      });
+      
+      $slider.slick({
+        slidesToShow: 1,
+        slidesToScroll: 1, 
+        arrows: false,
+        speed: 400,
+      });  
+      
+      setProgress(0);
+      
+
+  });
+
+
+
 
   
  
@@ -23,4 +55,3 @@ $(function () {
   
   
    
-});

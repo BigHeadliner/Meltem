d3a2eb66419e2f98c2fb598a8170a4ed2b1b1eab
BigHeadliner <bigheadliner@gmail.com>
2023-12-13 16:40:34 +0200

    form functionality almost ready

--- app/js/main.js
+++ app/js/main.js
@@ -31,20 +31,80 @@ $(function () {
       setProgress(0); 
         
        
-      $('#open-popup').on('click', function() {
-        $.magnificPopup.open({
-            items: {
-                src: '#popup',
-                type: 'inline'
-            },
-            closeOnBgClick: true // дозволяє закривати попап при кліку на оверлей
-        });
-    });
+    //   $('#open-popup').on('click', function() {
+    //     $.magnificPopup.open({
+    //         items: {
+    //             src: '#popup',
+    //             type: 'inline'
+    //         },
+    //         closeOnBgClick: true // дозволяє закривати попап при кліку на оверлей
+    //     });
+    // });
 
-    // Додано обробник подій для кнопки закриття попапу
-    $('#popup').on('click', '#close-popup', function() {
-        $.magnificPopup.close();
-    });
+    // // Додано обробник подій для кнопки закриття попапу
+    // $('#popup').on('click', '#close-popup', function() {
+    //     $.magnificPopup.close();
+    // }); 
+     
+    // Новий код для поапу 
+     
+  
+  // Початкове встановлення маски та показу маски при завантаженні сторінки
+  $('.phone-input').inputmask({ mask: '+380-99-999-99-99', showMaskOnHover: false, clearIncomplete: true });
+
+  // Додатковий код для відправлення форми та відображення попапів
+  $('#my-form').on('submit', function (e) {
+      e.preventDefault();
+
+      var name = $('#my-form input[name="name"]').val();
+      var phone = $('#my-form input[name="phone"]').val();
+
+      // Приклад валідації (ваш власний код)
+      if (name === '' || phone === '' || phone.indexOf('_') !== -1) {
+          // Підсвітка незаповненого інпута червоним кольором
+          $('#my-form input[name="name"], #my-form input[name="phone"]').css('border-color', 'red');
+          return;
+      }
+
+      // Ваш код для відправлення форми (наприклад, AJAX-запит)
+
+      // Закриття попапу
+      $.magnificPopup.close();
+
+      // Відобразити подяку
+      showThankYouPopup();
+  });
+
+  // Додано обробник подій для кнопки відкриття попапу
+  $('#open-popup').on('click', function () {
+      // Скидання підсвітки при відкритті попапу
+      $('#my-form input[name="name"], #my-form input[name="phone"]').css('border-color', '');
+
+      $.magnificPopup.open({
+          items: {
+              src: '#popup',
+              type: 'inline'
+          },
+          closeOnBgClick: true // дозволяє закривати попап при кліку на оверлей
+      });
+  });
+
+  // Додано обробник подій для кнопки закриття попапу
+  $('#popup').on('click', '#close-popup', function () {
+      $.magnificPopup.close();
+  });
+
+  // Функція для відображення подяки
+  function showThankYouPopup() {
+      $.magnificPopup.open({
+          items: {
+              src: '#thank-you-popup',
+              type: 'inline'
+          },
+          closeOnBgClick: true
+      });
+  }
+
    
      
   });
--- gulpfile.js
+++ gulpfile.js
@@ -54,7 +54,8 @@ function nunjucks(){
     'node_modules/magnific-popup/dist/jquery.magnific-popup.js', 
     'node_modules/rateyo/src/jquery.rateyo.js', 
     'node_modules/ion-rangeslider/js/ion.rangeSlider.js',  
-    'node_modules/jquery-form-styler/dist/jquery.formstyler.js', 
+    'node_modules/jquery-form-styler/dist/jquery.formstyler.js',  
+    'node_modules/inputmask/dist/jquery.inputmask.js', 
      'app/js/main.js'
    ]) 
    .pipe(concat('main.min.js')) 

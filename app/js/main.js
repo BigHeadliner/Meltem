1d1cf981d52b135f91f73ef2c861ebb010703324
BigHeadliner <bigheadliner@gmail.com>
2023-12-13 19:53:24 +0200

    two popup buttons work

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,21 @@
-$(function () {   
+$(function () {    
+   
+  const hamburger = document.getElementById('menu__btn')
+    const headerMenu = document.getElementById('menu__list')
+    const menuLinks = document.querySelectorAll('.menu__link') 
+  
+  
+    hamburger.addEventListener('click', () => {
+       headerMenu.classList.toggle('menu__list--active') 
+         hamburger.classList.toggle('menu__btn--open') 
+    })
+  
+    menuLinks.forEach(link => {
+      link.addEventListener('click', () => {
+      headerMenu.classList.remove('menu__list--active') 
+        hamburger.classList.remove('menu__btn--open')
+    })
+    })  
       
      
   
@@ -33,64 +50,137 @@ $(function () {
 
     // Новий код для поапу 
      
-  
-  // Початкове встановлення маски та показу маски при завантаженні сторінки
-  $('.phone-input').inputmask({ mask: '+380-99-999-99-99', showMaskOnHover: false, clearIncomplete: true });
+  // // Початкове встановлення маски та показу маски при завантаженні сторінки
+  // $('.phone-input').inputmask({ mask: '+380-99-999-99-99', showMaskOnHover: false, clearIncomplete: true });
 
-  // Додатковий код для відправлення форми та відображення попапів
-  $('#my-form').on('submit', function (e) {
-      e.preventDefault();
+  // // Додатковий код для відправлення форми та відображення попапів
+  // $('#my-form').on('submit', function (e) {
+  //     e.preventDefault();
 
-      var name = $('#my-form input[name="name"]').val();
-      var phone = $('#my-form input[name="phone"]').val();
+  //     var name = $('#my-form input[name="name"]').val();
+  //     var phone = $('#my-form input[name="phone"]').val();
 
-      // Приклад валідації (ваш власний код)
-      if (name === '' || phone === '' || phone.indexOf('_') !== -1) {
-          // Підсвітка незаповненого інпута червоним кольором
-          $('#my-form input[name="name"], #my-form input[name="phone"]').css('border-color', 'red');
-          return;
-      }
+  //     // Приклад валідації (ваш власний код)
+  //     if (name === '' || phone === '' || phone.indexOf('_') !== -1) {
+  //         // Підсвітка незаповненого інпута червоним кольором
+  //         $('#my-form input[name="name"], #my-form input[name="phone"]').css('border-color', 'red');
+  //         return;
+  //     }
 
-      // Ваш код для відправлення форми (наприклад, AJAX-запит)
+  //     // Ваш код для відправлення форми (наприклад, AJAX-запит)
 
-      // Закриття попапу
-      $.magnificPopup.close();
+  //     // Закриття попапу
+  //     $.magnificPopup.close();
 
-      // Відобразити подяку
-      showThankYouPopup();
-  });
+  //     // Відобразити подяку
+  //     showThankYouPopup();
+  // });
 
-  // Додано обробник подій для кнопки відкриття попапу
-  $('#open-popup').on('click', function () {
-      // Скидання підсвітки при відкритті попапу
-      $('#my-form input[name="name"], #my-form input[name="phone"]').css('border-color', '');
+  // // Додано обробник подій для кнопки відкриття попапу
+  // $('#open-popup').on('click', function () {
+  //     // Скидання підсвітки при відкритті попапу
+  //     $('#my-form input[name="name"], #my-form input[name="phone"]').css('border-color', '');
 
-      $.magnificPopup.open({
-          items: {
-              src: '#popup',
-              type: 'inline'
-          },
-          closeOnBgClick: true // дозволяє закривати попап при кліку на оверлей
-      });
-  });
+  //     $.magnificPopup.open({
+  //         items: {
+  //             src: '#popup',
+  //             type: 'inline'
+  //         },
+  //         closeOnBgClick: true // дозволяє закривати попап при кліку на оверлей
+  //     });
+  // });
 
-  // Додано обробник подій для кнопки закриття попапу
-  $('#popup').on('click', '#close-popup', function () {
-      $.magnificPopup.close();
-  });
+  // // Додано обробник подій для кнопки закриття попапу
+  // $('#popup').on('click', '#close-popup', function () {
+  //     $.magnificPopup.close();
+  // });
 
-  // Функція для відображення подяки
-  function showThankYouPopup() {
-      $.magnificPopup.open({
-          items: {
-              src: '#thank-you-popup',
-              type: 'inline'
-          },
-          closeOnBgClick: true
-      });
+  // // Функція для відображення подяки
+  // function showThankYouPopup() {
+  //     $.magnificPopup.open({
+  //         items: {
+  //             src: '#thank-you-popup',
+  //             type: 'inline'
+  //         },
+  //         closeOnBgClick: true
+  //     });
+  // }
+ 
+   
+  // НОВИЙ КОД ДЛЯ ПОПАПУ   
+    
+   // Початкове встановлення маски та показу маски при завантаженні сторінки
+$('.phone-input').inputmask({
+  mask: '+380-99-999-99-99',
+  showMaskOnHover: false,
+  clearIncomplete: true
+});
+
+// Додатковий код для відправлення форми та відображення попапів
+$('#my-form').on('submit', function (e) {
+  e.preventDefault();
+
+  var name = $('#my-form input[name="name"]').val();
+  var phone = $('#my-form input[name="phone"]').inputmask('unmaskedvalue'); // Отримати невідформатоване значення
+
+  // Приклад валідації (ваш власний код)
+  if (name === '' || phone === '' || phone.indexOf('_') !== -1) {
+      // Підсвітка незаповненого інпута червоним кольором
+      $('#my-form input[name="name"], #my-form input[name="phone"]').css('border-color', 'red');
+      return;
   }
 
-   
+  // Ваш код для відправлення форми (наприклад, AJAX-запит)
+
+  // Закриття попапу
+  $.magnificPopup.close();
+
+  // Відобразити подяку
+  showThankYouPopup();
+});
+
+// Додано обробник подій для кнопок відкриття попапу
+$('.header__contact-btn').on('click', function () {
+  // Скидання підсвітки при відкритті попапу
+  $('#my-form input[name="name"], #my-form input[name="phone"]').css('border-color', '');
+
+  $.magnificPopup.open({
+      items: {
+          src: '#popup',
+          type: 'inline'
+      },
+      closeOnBgClick: true // дозволяє закривати попап при кліку на оверлей
+  });
+});
+
+$('.header__contact-btn--spare').on('click', function () {
+  // Скидання підсвітки при відкритті попапу
+  $('#my-form input[name="name"], #my-form input[name="phone"]').css('border-color', '');
+
+  $.magnificPopup.open({
+      items: {
+          src: '#popup',
+          type: 'inline'
+      },
+      closeOnBgClick: true // дозволяє закривати попап при кліку на оверлей
+  });
+});
+
+// Додано обробник подій для кнопки закриття попапу
+$('#popup').on('click', '#close-popup', function () {
+  $.magnificPopup.close();
+});
+
+// Функція для відображення подяки
+function showThankYouPopup() {
+  $.magnificPopup.open({
+      items: {
+          src: '#thank-you-popup',
+          type: 'inline'
+      },
+      closeOnBgClick: true
+  });
+}
      
   });
 
